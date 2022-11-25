5973afac0daf74bad80b49ea4d0139c2bd629925
Iain <iain.hoolahan@gmail.com>
2022-11-25 15:23:13 +0000

    addScore and getScoreCard methods added to populate scorecard and access it

--- bowling-game.js
+++ bowling-game.js
@@ -4,7 +4,15 @@ class BowlingGame {
     this.scoreCard = []
   }
 
-  total(scoreCard) {
+  addScore(score) {
+    this.scoreCard.push(score);
+  }
+
+  getScoreCard() {
+    return this.scoreCard;
+  }
+
+  total() {
     const sum = [scoreCard].flat(2).reduce((partialSum, a) => partialSum + a, 0);
     return sum;
   }
